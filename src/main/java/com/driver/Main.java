5458ba388445558df6f3db7390d2febd44b720e5
chanchal kolekar <chanchalkolekar3@gmail.com>
2023-02-10 01:36:31 +0530

    created file and obj and encap

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // obj.name = "chanchal";
+
+        obj.setName("chanchal");
+        System.out.println(obj.getName());
+    }
 }
